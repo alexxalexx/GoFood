3f050ea88056f7fb58f3ebc15439f1f9cd44f550
alexxalexx <alexxkoltakovv@gmail.com>
2022-05-14 18:19:13 +0300

    Update 18:19

---
+++ GoFood.js
@@ -0,0 +1,12 @@
+let buttonA = document.getElementsByClassName(".image_2");
+let buttonB = document.getElementsByClassName(".image_1");
+
+buttonA.onclick = function()
+{
+    alert("Button A is pressed");
+};
+
+buttonB.onclick = function()
+{
+    alert("Button B is pressed");
+};
