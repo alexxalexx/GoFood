359235a028df5f329514586d31ca7d2cd5adcd48
alexxalexx <alexxkoltakovv@gmail.com>
2022-05-14 18:45:13 +0300

    Update 18:45

--- GoFood.js
+++ GoFood.js
@@ -1,5 +1,14 @@
+let deliveryButton = document.querySelector(".delivery_button");
+
 let buttonA = document.querySelector(".image_2");
 let buttonB = document.querySelector(".image_1");
+let buttonC = document.querySelector(".image_3");
+let buttonD = document.querySelector(".image_5");
+
+deliveryButton.onclick = function()
+{
+    alert("Delivery Button is pressed");
+};
 
 buttonA.onclick = function()
 {
@@ -10,3 +19,13 @@ buttonB.onclick = function()
 {
     alert("Button B is pressed");
 };
+buttonC.onclick = function()
+{
+    alert("Button С is pressed");
+};
+
+buttonD.onclick = function()
+{
+    alert("Button D is pressed");
+};
+
