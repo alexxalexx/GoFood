e16314f655a9e98b50308efa790c8247d3cd61d8
alexxalexx <alexxkoltakovv@gmail.com>
2022-05-21 19:01:33 +0300

    Update 19:01

--- GoFood.js
+++ GoFood.js
@@ -1,20 +1,6 @@
 let deliveryButton = document.querySelector(".delivery_button");
 
-let buttonPopularA = document.querySelector(".image_3");
-
-let buttonDairyA = document.querySelector(".image_12");
-
 deliveryButton.onclick = function()
 {
     prompt('Куда доставить?');
-};
-
-buttonPopularA.onclick = function()
-{
-    alert("Нажата категория 'Здоровый образ жизни'");
-};
-
-buttonDairyA.onclick = function()
-{
-    alert("Нажата категория 'Сыры'");
 };
